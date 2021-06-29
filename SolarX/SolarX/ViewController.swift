a6e34d668c995136e3a193afde390ae30761fa34
skhiearth <skhiearth@gmail.com>
2021-06-29 19:47:23 +0530

    project init
    Co-Authored-By: Simran Gogia <70960753+simmsss@users.noreply.github.com>

---
+++ SolarX/SolarX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SolarX
+//
+//  Created by Utkarsh Sharma on 29/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
